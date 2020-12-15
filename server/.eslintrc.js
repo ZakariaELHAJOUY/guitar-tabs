9fb9d30c8b4bba60fb2f4e361b3002470dabdf1c
zakaria-elhajouy <zakaria.hajouy@gmail.com>
2020-12-15 03:39:03 +0100

    setup of the express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "es6": true
+    },
+    "extends": "standard",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
